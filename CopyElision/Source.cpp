7486832c71f573f2b7c1b5a33b8f2c7b687f43a2
Ivaylo Donchev <idonchev@hotmail.com>
2016-11-29 16:42:19 +0200

    2

--- CopyElision/Source.cpp
+++ CopyElision/Source.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 struct Foo {
-	Foo() { std::cout << "Constructed" << std::endl; }
-	Foo(const Foo &) { std::cout << "Copy-constructed" << std::endl; }
-	Foo(Foo &&) { std::cout << "Move-constructed" << std::endl; }
-	~Foo() { std::cout << "Destructed" << std::endl; }
+	Foo() { std::cout << this << " Constructed" << std::endl; }
+	Foo(const Foo &) { std::cout << this << " Copy-constructed" << std::endl; }
+	Foo(Foo &&) { std::cout << this << " Move-constructed" << std::endl; }
+	~Foo() { std::cout << this << " Destructed" << std::endl; }
 };
 
 Foo f() 
@@ -13,9 +13,30 @@ Foo f()
 	return foo;
 }
 
+void ff()
+{
+	Foo foo;
+	throw foo;
+}
+
+void f(Foo f)
+{
+	std::cout << "Fn" << std::endl;
+}
 
 //Трябва да се компилира първо в debug, после в release
 int main() 
 {
 	Foo foo = f();
+	std::cout << std::endl;
+
+	f(Foo());
+	std::cout << std::endl;
+
+	try {
+		ff();
+	}
+	catch (Foo foo) {
+		std::cout << "Catch" << std::endl;
+	}
 }
